00b8129916e79f0a6b331d838310970e7708b9c5
Thomas Wilkinson <c2061425@my.shu.ac.uk>
2022-09-22 15:35:35 +0100

    1

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/news', (req, res) =>
     res.send('<p>some text</p>')
 )
 
+app.use((req, res, next)=>{
+    res.status(404).send("Sorry no page can be found")
+})
+
 app.listen(3000);
 
 console.log("Express on 3000");
